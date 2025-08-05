06310d580b7d76078dcd43bbe268b3a1292ef3a2
xairaven <alexkovalevkpi@gmail.com>
2025-08-05 05:10:23 +0300

    (FTB Revelation) Feat: Wipe script

---
+++ ftb-revelation-1.12.2/WIPE.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm ./crafttweaker.log
+rm -r ./logs/*
+
+rm ./luckperms/luckperms-h2.mv.db
+
+rm ./nucleus/usercache.json
+rm -r ./nucleus/userdata
+
+rm -r ./snapshots-server
+rm -r ./world
+
+rm ./usernamecache.json
+rm ./usercache.json
+rm ./whitelist.json
