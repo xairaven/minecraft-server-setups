e55f60811cd52c7bede4e6b8a2e2cbfe3db7f307
xairaven <alexkovalevkpi@gmail.com>
2025-08-05 19:10:05 +0300

    (FTB Revelation) Feat: Renamed start scripts

--- ftb-revelation-1.12.2/old_start.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-"jre/8.0.312+7/bin/java" -jar -Duser.timezone=Europe/Kyiv -Dfml.queryResult=confirm -Dlog4j.configurationFile=.patches/log4j2_112-116.xml -Xmx6144M forge-1.12.2-14.23.5.2860.jar nogui
